9c6c4481dfba0590ce0f41d95c8fed4d39d6b6eb
daisuke iba <d.iba@futurek.com>
2025-07-23 14:04:47 +0900

    repositoryNameを指定

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from 'next';
 
+const repositoryName = 'next-tiktok';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: `/${repositoryName}`,
+  assetPrefix: `/${repositoryName}`,
   trailingSlash: true,
   sassOptions: {
     additionalData: `$var: red;`,
